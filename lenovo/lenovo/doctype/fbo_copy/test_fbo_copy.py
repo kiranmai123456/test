8913db5334e8fa45532d896a522d15f25127f078
Kiranmai Machavarapu <kiranmai@easycloud.in>
2024-03-02 17:34:25 +0530

    testing

---
+++ lenovo/lenovo/doctype/fbo_copy/test_fbo_copy.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, kiranmai and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testfbocopy(FrappeTestCase):
+	pass
---
+++ lenovo/lenovo/doctype/vehicle_tracking_copy/vehicle_tracking_copy.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, kiranmai and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("vehicle tracking copy", {
+// 	refresh(frm) {
+
+// 	},
+// });
